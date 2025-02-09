88733df312065f82202f50111ac8f616281dfbf9
alii16 <boy932535@gmail.com>
2025-02-09 09:51:10 +0800

    first commit

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -8,22 +8,11 @@ class QRCodeController extends Controller
 {
     public function index()
     {
-        $text = 'hai'; // URL untuk QR Code
-        $fileName = 'qrcodes/' . md5($text) . '.png';
+        $text = 'haiii'; 
 
-        // Cek apakah QR Code sudah ada
-        if (!Storage::disk('public')->exists($fileName)) {
-            // Generate QR Code dengan GD Library
-            $qrCode = QrCode::format('png')->size(200)->generate($text);
+        // Generate QR Code
+        $qrCode = QrCode::size(200)->generate($text);
 
-            // Simpan QR Code di storage
-            Storage::disk('public')->put($fileName, $qrCode);
-        }
-
-        // Ambil URL QR Code dari storage
-        $qrCodeUrl = asset('storage/' . $fileName);
-
-        return view('welcome', compact('qrCodeUrl'));
+        return view('welcome', compact('qrCode'));
     }
 }
-
